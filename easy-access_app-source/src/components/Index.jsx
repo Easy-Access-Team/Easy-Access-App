e096e7239d10f0e0657117908949d67ea4968225
Axel Gomez Cordoba <2005045@utrivieramaya.edu.mx>
2024-02-16 23:54:48 -0500

    Componente de rutas y ruta comodín

---
+++ easy-access_app-source/src/components/Index.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const RouteList = () => {
+  
+    return <>
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Welcome/> }/>
+          <Route path="/login" element={ <Login/> } />
+          <Route path="/register" element={<Register/>}/> 
+          <Route path="/home" element={<Home/> } />
+          <Route path="/asignaciones" element={<Asignaciones/>}/>
+          <Route path="*" element={<Error404/>}/>
+        </Routes>
+      </Router>
+    </>
+  }
+  export default RouteList
+  
