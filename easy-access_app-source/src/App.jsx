3dfce69853450fca7bb465beb3a16a6b73876d4d
FabiolaCruzCipriano <2202185@utrivieramaya.edu.mx>
2024-02-18 11:27:17 -0500

    Crear theme provider

--- easy-access_app-source/src/App.jsx
+++ easy-access_app-source/src/App.jsx
@@ -1,11 +1,16 @@
+import { ThemeProvider } from "styled-components"
+import { lightTheme, darkTheme } from "../../UI/themes";
 import RouteList from "./components/Routes/Index.jsx"
+
+
+
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider>
     <RouteList />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ easy-access_app-source/src/UI/themes.js
@@ -0,0 +1,74 @@
+
+export const lightTheme = {
+    primary: "#4355b9",
+    onprimary: "#FFFFFF",
+    primarycont: "#dee0ff",
+    onprimarycont: "#00105c",
+
+    secondary: "#006877",
+    onsecondary: "#FFFFFF",
+    secondarycont: "#a2eeff",
+    onsecondarycont: "#001f25",
+
+    tertiary: "#6c5e00",
+    tertiarycont: "#fbe360",
+    ontertiarycont: "#211c00",
+
+    bg: "#fefbff",
+    onbg: "#1b1b1f",
+
+    error: "#ba1a1a",
+    onerror: "#ffffff",
+    errorcont: "#ffdad6",
+    onerrorcont: "#410002",
+
+    ok: "#426915",
+    okcont: "#c2f18e",
+    onokcont: "#0f2000",
+
+    alert: "#9f4200",
+    alertcont: "#ffdbcb",
+    onalertcont: "#341100",
+
+    outline: "#767680",
+    surfacev: "#F5EEFA",
+    onsurfv: "#46464f"
+}
+
+export const darkTheme = {
+    primary: "#bac3ff",
+    onprimary: "#08218a",
+    primarycont: "#293ca0",
+    onprimarycont: "#dee0ff",
+
+    secondary: "#51d7ef",
+    onsecondary: "#00363e",
+    secondarycont: "#004e5a",
+    onsecondarycont: "#a2eeff",
+
+    tertiary: "#ddc747",
+    tertiarycont: "#514700",
+    ontertiarycont: "#fbe360",
+
+    bg: "#1b1b1f",
+    onbg: "#e4e1e6",
+
+    error: "#ffb4ab",
+    onerror: "#690005",
+    errorcont: "#93000a",
+    onerrorcont: "#ffdad6",
+
+    ok: "#a7d475",
+    onok: "#1d3700",
+    okcont: "#2d5000",
+    onokcont: "#c2f18e",
+
+    alert: "#ffb692",
+    onalert: "#552000",
+    alertcont: "#793000",
+    onalertcont: "#ffdbcb",
+
+    outline: "#90909a",
+    surfacev: "#46464f",
+    onsurfv: "#c7c5d0"
+}
