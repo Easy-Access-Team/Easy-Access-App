04769b2789404717998080c386580a3524a73bdd
Axel Gomez Cordoba <2005045@utrivieramaya.edu.mx>
2024-02-28 20:25:24 -0500

    Home

---
+++ easy-access_app-source/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import AppTemplate from "../components/Template/Index";
+
+const Home = () => {
+    return <AppTemplate>
+        <h1>Bienvenido, usuario.</h1>
+    </AppTemplate>
+}
+
+export default Home;
