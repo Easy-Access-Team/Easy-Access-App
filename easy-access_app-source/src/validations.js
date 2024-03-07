86e45f9a84f41eeee13aac4ce8876290e7c5ff6c
AugustoJmnz <2002010@utrivieramaya.edu.mx>
2024-03-06 22:15:02 -0500

    Validacion de inputs login

--- easy-access_app-source/src/validations.js
+++ easy-access_app-source/src/validations.js
@@ -7,6 +7,7 @@ export const validateEmail = (value) => {
         return {fail: true, description: "Ingresa un correo valido: nombredeusuario@dominio.com."}
     }
 }
+
 export const validatePass = (value) => {
     const pat = /^(?=.[A-Z])(?=.[a-z])(?=.[0-9])(?=.[@#$%&/])[A-Za-z0-9@#$%&/]{8,12}$/;
     if(pat.test(value)){
@@ -15,4 +16,3 @@ export const validatePass = (value) => {
         return {fail: true, description: "Se requiere: Minimo una Mayúscula, una Minúscula y un Número con al menos uno de estos caracteres @#$%& y una logitud de 8 a 12 caracteres."}
     }
 }
-
