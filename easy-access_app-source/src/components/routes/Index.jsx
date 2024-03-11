b5561ed83347f5fc1f231f020a1da8de0c16f375
Axel Gomez Cordoba <2005045@utrivieramaya.edu.mx>
2024-03-11 13:46:33 -0500

    Integracion de facebook en register

--- easy-access_app-source/src/components/routes/Index.jsx
+++ easy-access_app-source/src/components/routes/Index.jsx
@@ -26,7 +26,7 @@ const RouteList = () => {
       <Routes>
         <Route path="/" element={ <Welcome toggleTheme={toggleTheme} tema={tema} /> }/>
         <Route path="/login" element={ <Login action={login} auth={auth} toggleTheme={toggleTheme} google={loginWithGoogle} facebook={loginWithFacebook} tema={tema}/> } />
-        <Route path="/register" element={<Register action={signUp} google={loginWithGoogle} toggleTheme={toggleTheme} tema={tema} auth={auth}/>}/> 
+        <Route path="/register" element={<Register action={signUp} google={loginWithGoogle} toggleTheme={toggleTheme} tema={tema} auth={auth} facebook={loginWithFacebook} />}/> 
         {/* <Route path="/home" element={<Home/> } />
         <Route path="/asignaciones" element={<Asignaciones/>}/>
         <Route path="*" element={<Error404/>}/> */}
