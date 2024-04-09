9f719fa8c7c25eebefe6c110483ec52bf8067ea9
FabiolaCruzCipriano <2202185@utrivieramaya.edu.mx>
2024-04-09 18:35:58 -0500

    Suscripcion del usuario

--- Aditum Delta Source/src/components/Routes/Index.jsx
+++ Aditum Delta Source/src/components/Routes/Index.jsx
@@ -28,6 +28,7 @@ const RouteList = () => {
     <Loader message={loader}/>
     <Router>
       <Routes>
+        <Route path="suscription" element={<Suscription />} />
         <Route path="perfil" element={<Profile/>}/>
         <Route path="/welcome" element={ <Welcome/> }/>
         <Route path="/auth/" element={<AuthContainer/>}>
